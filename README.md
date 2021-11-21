e100fd6032c528408c7ec67b92f8aec25118f83c
lishenjian <lsjpostbox@163.com>
2021-11-21 16:26:05 +0800

    处理post请求

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-"# blog-server" 
+# blog-server
+# GET请求
+- 浏览器可以模拟GET请求（无法模拟POST请求，使用postman 获取 ajax 请求）
--- app.js
+++ app.js
@@ -1,16 +1,31 @@
 const http = require('http');
 const querystring = require('querystring');
 
-const server = http.createServer((req, res)=>{
-  //浏览器直接访问可以发起GET请求
-  console.log('method',req.method);
-  const url = req.url;
-  console.log('url', url);
+// const server = http.createServer((req, res)=>{
+//   //浏览器直接访问可以发起GET请求
+//   console.log('method',req.method);
+//   const url = req.url;
+//   console.log('url', url);
 
-  req.query = querystring.parse(url.split('?')[1])
-  console.log('query', req.query);
-  res.end(JSON.stringify(req.query))
+//   req.query = querystring.parse(url.split('?')[1])
+//   console.log('query', req.query);
+//   res.end(JSON.stringify(req.query))
+// })
+
+const server = http.createServer((req,res)=>{
+  if(req.method == 'POST') {
+    console.log('content-type:', req.headers['content-type']);
+    let postData = '';
+    req.on('data', chunk=>{
+      postData += chunk.toString();
+    })
+    req.on('end', ()=>{
+      console.log('postData', postData);
+      res.end(postData)
+    })
+  }
 })
+
 
 server.listen(8000)
 console.log('ok');
